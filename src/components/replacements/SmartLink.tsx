0acdbd815b7cf48eece5c260aaae674d47075f0e
Jake Bukuts <Jake Bukuts>
2023-04-07 18:04:18 -0400

    remove prefixed path

--- src/components/replacements/SmartLink.tsx
+++ src/components/replacements/SmartLink.tsx
@@ -1,4 +1,4 @@
-import { Link } from '@reach/router';
+import { Link as GatsbyLink } from 'gatsby';
 import React from 'react';
 import { Launch } from '@carbon/react/icons';
 import { Link as CarbonLink } from '@carbon/react';
@@ -11,7 +11,7 @@ const SmartLink = (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
   const isInternal =
     !href.startsWith('//') && !href.startsWith('http') && !href.startsWith('mailto:');
 
-  if (isInternal) return <Link to={href}>{children}</Link>;
+  if (isInternal) return <GatsbyLink to={href}>{children}</GatsbyLink>;
 
   return (
     <CarbonLink href={href} target='_blank' rel='noreferrer'>
